26645a8fb55e2ecef66b1254ee758cc14746037a
Domynis <54539177+Domynis@users.noreply.github.com>
2025-04-29 19:26:28 +0300

    Add tfjs model

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import copy from 'rollup-plugin-copy'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    copy({
+      targets: [
+        { src: 'model_tfjs/*', dest: 'dist/model_tfjs' }
+      ],
+      hook: 'writeBundle'
+    })
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules/@tensorflow')) {
+            return 'tensorflow'
+          }
+        }
+      }
+    },
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
+  },
+  optimizeDeps: {
+    include: ['@tensorflow/tfjs']
+  },
+  define: {
+    'process.env': {}
+  }
 })
